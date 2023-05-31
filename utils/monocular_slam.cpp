7116ab0f83d138c93d8aa39eb3715dc890cda4cb
vnmsklnk <st086866@student.spbu.ru>
2023-05-31 23:37:04 +0300

    Mod for saving trajectory and markers' poses

--- utils/monocular_slam.cpp
+++ utils/monocular_slam.cpp
@@ -23,6 +23,84 @@
 #include "map.h"
 #include "inputreader.h"
 #include "basictypes/cvversioning.h"
+#include <iostream>
+#include <fstream>
+
+inline float SIGN(float x) {
+    return (x >= 0.0f) ? +1.0f : -1.0f;
+}
+
+inline float NORM(float a, float b, float c, float d) {
+    return sqrt(a * a + b * b + c * c + d * d);
+}
+
+// quaternion = [x, y, z, w]'
+cv::Mat mRot2Quat(const cv::Mat& m) {
+    float r11 = m.at<float>(0, 0);
+    float r12 = m.at<float>(0, 1);
+    float r13 = m.at<float>(0, 2);
+    float r21 = m.at<float>(1, 0);
+    float r22 = m.at<float>(1, 1);
+    float r23 = m.at<float>(1, 2);
+    float r31 = m.at<float>(2, 0);
+    float r32 = m.at<float>(2, 1);
+    float r33 = m.at<float>(2, 2);
+    float q0 = (r11 + r22 + r33 + 1.0f) / 4.0f;
+    float q1 = (r11 - r22 - r33 + 1.0f) / 4.0f;
+    float q2 = (-r11 + r22 - r33 + 1.0f) / 4.0f;
+    float q3 = (-r11 - r22 + r33 + 1.0f) / 4.0f;
+    if (q0 < 0.0f) {
+        q0 = 0.0f;
+    }
+    if (q1 < 0.0f) {
+        q1 = 0.0f;
+    }
+    if (q2 < 0.0f) {
+        q2 = 0.0f;
+    }
+    if (q3 < 0.0f) {
+        q3 = 0.0f;
+    }
+    q0 = sqrt(q0);
+    q1 = sqrt(q1);
+    q2 = sqrt(q2);
+    q3 = sqrt(q3);
+    if (q0 >= q1 && q0 >= q2 && q0 >= q3) {
+        q0 *= +1.0f;
+        q1 *= SIGN(r32 - r23);
+        q2 *= SIGN(r13 - r31);
+        q3 *= SIGN(r21 - r12);
+    }
+    else if (q1 >= q0 && q1 >= q2 && q1 >= q3) {
+        q0 *= SIGN(r32 - r23);
+        q1 *= +1.0f;
+        q2 *= SIGN(r21 + r12);
+        q3 *= SIGN(r13 + r31);
+    }
+    else if (q2 >= q0 && q2 >= q1 && q2 >= q3) {
+        q0 *= SIGN(r13 - r31);
+        q1 *= SIGN(r21 + r12);
+        q2 *= +1.0f;
+        q3 *= SIGN(r32 + r23);
+    }
+    else if (q3 >= q0 && q3 >= q1 && q3 >= q2) {
+        q0 *= SIGN(r21 - r12);
+        q1 *= SIGN(r31 + r13);
+        q2 *= SIGN(r32 + r23);
+        q3 *= +1.0f;
+    }
+    else {
+        printf("coding error\n");
+    }
+    float r = NORM(q0, q1, q2, q3);
+    q0 /= r;
+    q1 /= r;
+    q2 /= r;
+    q3 /= r;
+
+    cv::Mat res = (cv::Mat_<float>(4, 1) << q1, q2, q3, q0);
+    return res;
+}
 
 
 cv::Mat getImage(cv::VideoCapture &vcap,int frameIdx){
@@ -222,6 +300,7 @@ int main(int argc,char **argv){
         bool finish = false;
         cv::Mat camPose_c2g;
         int vspeed=stoi(cml("-vspeed","1"));
+        ofstream TrajectoryFile("trajectory.txt");
         while (!finish && !in_image.empty()) {
             //image resize (if required)
             in_image = resize(in_image, vsize);
@@ -240,6 +319,19 @@ int main(int argc,char **argv){
             camPose_c2g=Slam.process(in_image, image_params,currentFrameIndex);
             Fps.stop();
 
+            if(camPose_c2g.empty()){
+                TrajectoryFile<<"0 0 0 0 0 0 1"<<endl;
+            }
+            else{
+                auto quat = mRot2Quat(camPose_c2g);
+                TrajectoryFile<<camPose_c2g.at<float>(0, 3)<<" "
+                <<camPose_c2g.at<float>(1, 3)<<" "
+                <<camPose_c2g.at<float>(2, 3)<<" "
+                <<quat.at<float>(0, 0)<<" "
+                <<quat.at<float>(1, 0)<<" "
+                <<quat.at<float>(2, 0)<<" "
+                <<quat.at<float>(3, 0)<<endl;
+            }
 
             cout << "Image " << currentFrameIndex << " fps=" << 1./Fps.getAvrg()<< endl;
             //            Slam.drawMatches(in_image);
@@ -290,6 +382,22 @@ int main(int argc,char **argv){
             Slam.saveToFile("slam.slm");
         }
         TheMap->saveToMarkerMap("markermap.yml");
+
+        ofstream MarkersFile("markers.txt");
+        for(const auto &marker:TheMap->map_markers){
+            auto pose = marker.second.pose_g2m;
+            if(pose.isValid()){
+                auto quat = mRot2Quat(pose);
+                MarkersFile<<marker.second.id<<" "
+                <<pose.at<float>(0, 3)<<" "
+                <<pose.at<float>(1, 3)<<" "
+                <<pose.at<float>(2, 3)<<" "
+                <<quat.at<float>(0, 0)<<" "
+                <<quat.at<float>(1, 0)<<" "
+                <<quat.at<float>(2, 0)<<" "
+                <<quat.at<float>(3, 0)<<endl;
+            }
+        }
 
 
     }
